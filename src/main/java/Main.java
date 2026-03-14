fd4fbf3b073c9536978126412e685f98e1900732
cququantum <2256839542@qq.com>
2026-03-14 13:37:26 +0800

    lab-final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
     private static final double RESULT_TOL = CplexConfig.MIP_GAP;
 
     public static void main(String[] args) {
-        SolverMode solverMode = SolverMode.ALL;
+        SolverMode solverMode = SolverMode.LBBD_NO_INIT;
         int startArg = 0;
         if (args.length > 0 && args[0].startsWith("--solver=")) {
             solverMode = parseSolverMode(args[0].substring("--solver=".length()));
@@ -26,7 +26,7 @@ public class Main {
             instancePaths = new String[args.length - startArg];
             System.arraycopy(args, startArg, instancePaths, 0, instancePaths.length);
         } else {
-            instancePaths = new String[]{"data/MVPRP/MVPRP2_10_3_2.txt"};
+            instancePaths = new String[]{"data/MVPRP/MVPRP3_10_9_3.txt"};
         }
 
         for (int idx = 0; idx < instancePaths.length; idx++) {
